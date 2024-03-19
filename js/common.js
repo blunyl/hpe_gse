d44e7a9adc242fae4fa754957c5cf116ba43a69c
seg <142004071+blunyl@users.noreply.github.com>
2024-03-19 15:15:11 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".pg",
+  },
+});
